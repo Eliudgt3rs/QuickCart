e3ed8707bb89e3f78ac8e73367a5449ab8ed3c45
Eiud Waititu <eliudgachuki130@gmail.com>
2025-09-25 10:40:28 +0300

    ADDED SITEMAP

---
+++ app/sitemap.js
@@ -0,0 +1,51 @@
+
+import { blogs } from '@/data/blogs';
+import connectDB from '@/config/db';
+import Product from '@/models/Product';
+
+const URL = 'https://yenuphoneskenya.com';
+
+export default async function sitemap() {
+    await connectDB();
+    const products = await Product.find({});
+
+    const productEntries = products.map(({ _id, date }) => ({
+        url: `${URL}/product/${_id}`,
+        lastModified: new Date(date).toISOString(),
+        changeFrequency: 'daily',
+    }));
+
+    const blogEntries = blogs.map(({ id, date }) => ({
+        url: `${URL}/blog/${id}`,
+        lastModified: new Date(date).toISOString(),
+        changeFrequency: 'daily',
+    }));
+
+    const staticPaths = [
+        '/',
+        '/about-us',
+        '/add-address',
+        '/all-products',
+        '/blog',
+        '/cancel',
+        '/cart',
+        '/contact-us',
+        '/order-placed',
+        '/search-results',
+        '/seller',
+        '/seller/orders',
+        '/seller/product-list',
+        '/success',
+        '/wishlist',
+    ].map(path => ({
+        url: `${URL}${path}`,
+        lastModified: new Date().toISOString(),
+        changeFrequency: 'daily',
+    }));
+
+    return [
+        ...staticPaths,
+        ...productEntries,
+        ...blogEntries,
+    ];
+}
